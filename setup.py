473fe4216047612b4b520d20d43c6f7d2f2b8ebf
david <davidtnfsh@gmail.com>
2018-03-15 10:16:55 +0800

    forgot to update version num...

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
         name='opencc-python-reimplemented',
-        version='0.1.1',
+        version='0.1.2',
         description='OpenCC made with Python',
         long_description=readme(),
         url='https://github.com/yichen0831/opencc-python',
